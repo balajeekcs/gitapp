447485c3f838b1f6b35bc66d1fa225ef6f7a3f8f
balajeekcs <balajee_kcs@outlook.com>
2026-03-13 14:49:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
-resource "local_file" "test" {
-  content  = "Hello Bala Terraform Practice"
-  filename = "hello.txt"
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "~> 2.4"
+    }
+  }
+}
+
+provider "local" {}
+
+resource "local_file" "terraform_test" {
+  filename = "devops-practice.txt"
+  content  = "Terraform Practice by Bala"
 }
