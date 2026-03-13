66c29c2718c12217a6997f3add6957830efb69d2
balajeekcs <balajee_kcs@outlook.com>
2026-03-13 16:14:32 +0530

    Add variables for filename and content in Terraform

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "filename" {
+  description = "File name"
+  default     = "terraform-devops.txt"
+}
+
+variable "content" {
+  description = "File content"
+  default     = "Learning Terraform step by step"
+}
